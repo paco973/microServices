5cac7ecd51e4e02bd86d876285969a857ea65bc8
paco973 <djopaco973@gmail.com>
2022-01-28 11:40:57 +0100

    docker

--- MovieRater/movierater/settings.py
+++ MovieRater/movierater/settings.py
@@ -82,17 +82,23 @@ WSGI_APPLICATION = 'movierater.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'micro',
+#         'USER': 'paco',
+#         'PASSWORD': 'Test5910',
+#         'HOST': 'pgsql',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'micro',
-        'USER': 'paco',
-        'PASSWORD': 'Test5910',
-        'HOST': 'pgsql',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'movierater_database', # This is where you put the name of the db file.
     }
 }
-
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': {
